8d6d19afadf5eeec11c2814db4aa674de227b244
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2024-10-23 11:48:17 +0530

    Update areplyraid.py

--- RAUSHAN/plugins/spam/areplyraid.py
+++ RAUSHAN/plugins/spam/areplyraid.py
@@ -12,7 +12,7 @@ from pyrogram import Client, errors, filters
 from pyrogram.types import ChatPermissions, Message
 DEVS = int(7403621976)
 from RAUSHAN.helper.PyroHelpers import get_ub_chats
-from RAUSHAN.modules.basic.profile import extract_user, extract_user_and_reason
+from RAUSHAN.plugins.basic.profile import extract_user, extract_user_and_reason
 SUDO_USERS = SUDO_USER
 from .replyraid import RAIDS
 
