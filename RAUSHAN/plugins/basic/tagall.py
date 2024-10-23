616c5e4ac6f70a2ad75d9a197853983f5ee0731d
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2024-10-23 11:32:47 +0530

    Update tagall.py

--- RAUSHAN/plugins/basic/tagall.py
+++ RAUSHAN/plugins/basic/tagall.py
@@ -4,7 +4,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 
 
-from RAUSHAN.modules.help import add_command_help
+from RAUSHAN.plugins.help import add_command_help
 
 spam_chats = []
 
