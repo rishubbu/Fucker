8ba42f0e36a66836c07374e889a34bcad3f78c91
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2024-10-23 11:44:21 +0530

    Update global.py

--- RAUSHAN/plugins/globals/global.py
+++ RAUSHAN/plugins/globals/global.py
@@ -2,10 +2,10 @@ from pyrogram import Client, errors, filters
 from pyrogram.types import ChatPermissions, Message
 DEVS = [7037832227, 5959308905]
 from RAUSHAN.helper.PyroHelpers import get_ub_chats
-from RAUSHAN.modules.basic.profile import extract_user, extract_user_and_reason
+from RAUSHAN.plugins.basic.profile import extract_user, extract_user_and_reason
 from RAUSHAN.database import gbandb as RAUSHAN
 from RAUSHAN.database import gmutedb as Gmute
-from RAUSHAN.modules.help import add_command_help
+from RAUSHAN.plugins.help import add_command_help
 
 ok = []
 
