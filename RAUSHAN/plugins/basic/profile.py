bccd00cd646a1a26b69ac8aa2ea00bf555d01f0c
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2024-10-23 10:55:44 +0530

    Update profile.py

--- RAUSHAN/plugins/basic/profile.py
+++ RAUSHAN/plugins/basic/profile.py
@@ -12,7 +12,7 @@ from pyrogram.types import Message
 from RAUSHAN import SUDO_USER
 from RAUSHAN.helper.PyroHelpers import ReplyCheck
 
-from RAUSHAN.modules.help import add_command_help
+from RAUSHAN.plugins.help import add_command_help
 
 flood = {}
 profile_photo = "cache/pfp.jpg"
